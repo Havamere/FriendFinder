874a8505ebd217ba07d826501384d5af7cdb2789
Kyle R <kyle.rinehart83@gmail.com>
2016-06-27 18:16:01 -0400

    starting on in page javascript

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@ var friends = [
 	{
 		"name": "Ace",
 		"photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwioisSv_cHNAhVCXB4KHYU-AUcQjRwIBw&url=http%3A%2F%2Fbasementrejects.com%2Freview%2Face-ventura-when-nature-calls-1995%2F&psig=AFQjCNFsOJIa-XbHrjyX0xlKYSOmFwjRvw&ust=1466902896912624",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
+		"scores": [5,4,3,1,3,1,3,3,5,5]
 	},
 	{
 		"name": "Rocky",
 		"photo": "https://media.timeout.com/images/140701/617/347/image.jpg",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
+		"scores": [1,5,4,1,4,5,1,4,5,4]
 	},
 	{
 		"name": "Biff",
 		"photo": "http://cdn.hitfix.com/photos/6057863/Big_Trouble_In_Little_China_remake_news_article_story_large.jpg",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
+		"scores": [2,2,5,5,3,4,3,1,2,3]
 	},
 	{
 		"name": "Bean",
 		"photo": "https://anglotees.com/wp-content/uploads/2014/12/r4q1gawgmeoeglkuync6.jpg",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
+		"scores": [4,3,3,5,5,3,5,3,1,4]
 	},
 	{
 		"name": "Barf",
 		"photo": "http://screeninvasion.com/wp-content/uploads/2012/08/Spaceballs_John_Candy_Barf.jpg",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
+		"scores": [5,1,5,2,5,2,4,2,4,1]
 	}
 ];
 //sends to server
