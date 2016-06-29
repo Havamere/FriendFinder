76898fdab871770988d023669ca6244ef3d3796f
Kyle R <kyle.rinehart83@gmail.com>
2016-06-29 16:16:34 -0400

    comments added

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -22,12 +22,13 @@ module.exports = function(app) {
 		//console.log(req.body);
 		//console.log(friendsList);
 
-		//functions to return best match
+		//returned friend value
 		var bestMatch = {
 			'name': 'none',
 			'photo': 'none'
 		};
 
+		//allows the addition of an array into a single number
 		function sum (array) {
 			var total = 0;
 			for (var n = 0; n < array.length; n++) {
@@ -38,13 +39,14 @@ module.exports = function(app) {
 			return total;
 		}
 
+		//comparative number for user's array total
 		var userTotal = sum(req.body.scores);
+		//confirms sum of user's array
+		//console.log(userTotal);
 
-
-		console.log(userTotal);
-
+		//set outside of loops to be mutable and resetable
 		var friendTotal = 0;
-
+		//functions to return exact match
 		for (var i = 0; i < friendsList.length; i++) {
 			friendTotal = sum(friendsList[i].scores);
 			//console.log(friendTotal);
@@ -54,9 +56,12 @@ module.exports = function(app) {
 			}
 		};
 
+		//runs if exact match is not found
 		if (bestMatch.name == 'none') {
+			//highest possible amount score array can equal
 			var closest = 50;
-
+			//function to loop though array of friends and attempt to find friend sum closest to user sum
+			//should only update bestMatch when a closer sum is found
 			for (var i = 0; i < friendsList.length; i++) {
 				friendTotal = sum(friendsList[i].scores);
 				var difference = Math.abs(friendTotal - userTotal);
@@ -67,7 +72,10 @@ module.exports = function(app) {
 				};
 			};
 		};
+
+		//test answer
 		console.log(bestMatch);
+		//send bestMatch back to webpage
 		res.json(bestMatch);
 
 	});
